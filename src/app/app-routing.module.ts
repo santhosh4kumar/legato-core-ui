9f1d8cab6dc773f622b7bc58249abf8d5ad5c3ca
Maharana <AF83580@ad.wellpoint.com>
2019-05-17 19:15:59 +0530

    initial errors removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,45 +1,7 @@
-import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from './guards/auth.guard';
-import { CoursesComponent } from './courses/courses.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, CanActivate } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { LoginComponent } from './login/login.component';
-import { CartComponent } from './cart/cart.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { 
-    path: 'courses', 
-    component:  CoursesComponent, 
-    canActivate: [AuthGuard]
-  },
-  { 
-    path: 'course/:id', 
-    component:  CoursesComponent, 
-    canActivate: [AuthGuard]
-  },
-  { path: 'about', component:  AboutComponent},
-  { path: 'contact', component:  ContactComponent},
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
-  },
-  { path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
